377bce073ef22ca9b08981da5178f2c69cca0a81
Howisthisaface <Howisthisaface@users.noreply.github.com>
2020-05-13 11:28:14 -0400

    Completed_Roll_A_Ball

---
+++ Assets/Scripts/RotatePickUp.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePickUp : MonoBehaviour
+{
+    void Update() => transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+}
